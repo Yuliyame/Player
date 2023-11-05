f80320ef90ba970b15b25945f55bf6b5425254c3
YuliyaYuliya <yr555556666667777777@gmail.com>
2023-11-05 17:33:11 +0300

    Player and Game added

---
+++ src/main/java/Player.java
@@ -0,0 +1,17 @@
+public class Player {
+    protected int id;
+    protected String name;
+    protected int strength;
+
+    public Player(int id, String name, int strength) {
+        this.id;
+        this.name;
+        this.strength;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getStrength() {
+        return strength;
+    }
+}
